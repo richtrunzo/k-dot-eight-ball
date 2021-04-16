811b3895b3e846776a6cf2940d3625160d282edb
Rtrunzo <richtrunzo@gmail.com>
2021-04-16 10:07:15 -0700

    Added images and replies via interface, which are rendered randomly to the dom via a helper function.

--- src/App.tsx
+++ src/App.tsx
@@ -2,36 +2,46 @@ import React, {useState} from 'react';
 import './App.css';
 import Navbar from './Nav'
 
+let replies = ['test',
+               'testtest',
+               'test3',
+               'test4',
+               'test5',
+               'test6',
+               'test7',
+               'test8']
+
+let images = ['./images/Damn-album.jpeg',
+              './images/k-dot-blue.jpeg',
+              './images/k-dot-car.jpeg',
+              './images/k-dot-chair.jpeg',
+              './images/k-dot-stage.jpeg',
+              './images/k-dot-young.jpeg',
+              './images/kdot-mic.jpeg',
+              './images/tpab-album.jpeg']
+
+interface Answers {
+  image: string;
+  text: string
+}
+
+function Random(answer: Answers, num: number) {
+    let objNumber =  Math.floor(Math.random() * num);
+    answer.image = images[objNumber]
+    answer.text = replies[objNumber]
+    return answer
+}
+
 
 function App() {
 let [question, setQuestion] = useState(false);
 let [askedQ, setAskedQ] = useState('');
 
-let answers = {
-  1: '',
-  2: '',
-  3: '',
-  4: '',
-  5: '',
-  6: '',
-  7: '',
-  8: ''
-}
-
-let images = {
-  1: '',
-  2: '',
-  3: '',
-  4: '',
-  5: '',
-  6: '',
-  7: '',
-  8: ''
-}
 
 let formSubmit = () => {
   setQuestion(true)
 }
+
 
 let restart = () => {
   setQuestion(false)
@@ -45,13 +55,20 @@ let setAsked = (e: React.FormEvent<HTMLInputElement>) => {
 
 
 if (question === true) {
+  let kdot =  {
+    image: '',
+    text: ''
+  }
+  let x = Random(kdot, 9)
+  console.log(x)
+
   return (
     <>
     <Navbar />
     <div>
-      <img></img>
+      <img src={x.image}></img>
       <p>{askedQ}</p>
-      <p>answer</p>
+      <p>{x.text}</p>
       <button onClick={restart}></button>
     </div>
     </>
